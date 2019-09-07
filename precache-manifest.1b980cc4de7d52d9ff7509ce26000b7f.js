e9b3f9ea9074cb2737943ab1b881cffe710a8f2b
Nicolas Hollmann <43784252+NHollmann@users.noreply.github.com>
2019-09-07 20:10:42 +0200

    Updates

---
+++ precache-manifest.1b980cc4de7d52d9ff7509ce26000b7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d947e9feb35525950d9977c2ebc5eba8",
+    "url": "/dtfm-tool/index.html"
+  },
+  {
+    "revision": "25c483732555c92d81ab",
+    "url": "/dtfm-tool/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "6ffa1fb0471bb8a9c4db",
+    "url": "/dtfm-tool/static/js/2.f15ae829.chunk.js"
+  },
+  {
+    "revision": "25c483732555c92d81ab",
+    "url": "/dtfm-tool/static/js/main.3f50b0fd.chunk.js"
+  },
+  {
+    "revision": "01af627b23c68de8cd76",
+    "url": "/dtfm-tool/static/js/runtime~main.781366b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dtfm-tool/static/media/logo.5d5d9eef.svg"
+  }
+]);
